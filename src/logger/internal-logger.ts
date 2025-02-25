f62af1c761a5e71693731fe1f8b424474658bc91
szymeo <dev.sgracki@gmail.com>
2025-02-25 18:21:49 +0100

    fix: support older projects

--- src/logger/internal-logger.ts
+++ src/logger/internal-logger.ts
@@ -1,6 +1,6 @@
 import { Logger } from './Logger.js';
 import chalk from 'chalk';
-import pkgJson from '../../package.json' with { type: 'json' };
+import pkgJson from '../../package.json';
 
 export const internalLogger = new Logger(console.log, () =>
 	chalk.magenta(`\n[LogDash@${pkgJson.version}]\n`),
