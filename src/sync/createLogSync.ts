018be6775fb5588633c9d538bd9c52a2e569dc94
ablaszkiewicz <kqmdjc8@gmail.com>
2025-03-16 15:01:17 +0100

    feat(): additional log info

--- src/sync/createLogSync.ts
+++ src/sync/createLogSync.ts
@@ -6,7 +6,7 @@ import { NoopLogSync } from './NoopLogSync.js';
 export const createLogSync = (API_KEY?: string): LogSync => {
 	if (!API_KEY) {
 		internalLogger.log(
-			'Api key was not provided, using only local logger.\n',
+			'Api key was not provided in the InitializationParams when calling createLogdash(), using only local logger.\n',
 		);
 		return new NoopLogSync();
 	}
