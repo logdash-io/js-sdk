0a2e8530bf4859b70d742cee02c5a10b8fcd7a32
Aleksander Błaszkiewicz <aleksander.blaszkiewicz@flip.shop>
2025-05-13 18:03:18 +0200

    feat: stringify json arguments

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -20,35 +20,50 @@ export class Logger {
 	) {}
 
 	error(...data: any[]): void {
-		this._log(LogLevel.ERROR, data.join(' '));
+		this._log(LogLevel.ERROR, this._formatData(data));
 	}
 
 	warn(...data: any[]): void {
-		this._log(LogLevel.WARN, data.join(' '));
+		this._log(LogLevel.WARN, this._formatData(data));
 	}
 
 	info(...data: any[]): void {
-		this._log(LogLevel.INFO, data.join(' '));
+		this._log(LogLevel.INFO, this._formatData(data));
 	}
 
 	log(...data: any[]): void {
-		this._log(LogLevel.INFO, data.join(' '));
+		this._log(LogLevel.INFO, this._formatData(data));
 	}
 
 	http(...data: any[]): void {
-		this._log(LogLevel.HTTP, data.join(' '));
+		this._log(LogLevel.HTTP, this._formatData(data));
 	}
 
 	verbose(...data: any[]): void {
-		this._log(LogLevel.VERBOSE, data.join(' '));
+		this._log(LogLevel.VERBOSE, this._formatData(data));
 	}
 
 	debug(...data: any[]): void {
-		this._log(LogLevel.DEBUG, data.join(' '));
+		this._log(LogLevel.DEBUG, this._formatData(data));
 	}
 
 	silly(...data: any[]): void {
-		this._log(LogLevel.SILLY, data.join(' '));
+		this._log(LogLevel.SILLY, this._formatData(data));
+	}
+
+	private _formatData(data: any[]): string {
+		return data
+			.map((item) => {
+				if (typeof item === 'object' && item !== null) {
+					try {
+						return JSON.stringify(item);
+					} catch (e) {
+						return String(item);
+					}
+				}
+				return String(item);
+			})
+			.join(' ');
 	}
 
 	private _log(level: LogLevel, message: string): void {
